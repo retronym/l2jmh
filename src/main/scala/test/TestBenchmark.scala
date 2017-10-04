c60f45e23410844e9299c8e9160665cbf599e855
Dale Wijnand <dale.wijnand@gmail.com>
2017-10-04 15:04:10 +0100

    pat mat 2

--- src/main/scala/test/TestBenchmark.scala
+++ src/main/scala/test/TestBenchmark.scala
@@ -13,12 +13,19 @@ class TestBenchmark {
     case Some(c) => c.isLower
     case None    => false
   }
+  def implPatMat2(x: Option[Char]) = x match {
+    case None    => false
+    case Some(c) => c.isLower
+  }
 
   @Benchmark def foldSome(): Boolean = implFold(someChar)
   @Benchmark def foldNone(): Boolean = implFold(noneChar)
 
   @Benchmark def patMatSome(): Boolean = implPatMat(someChar)
   @Benchmark def patMatNone(): Boolean = implPatMat(noneChar)
+
+  @Benchmark def patMatSome2(): Boolean = implPatMat2(someChar)
+  @Benchmark def patMatNone2(): Boolean = implPatMat2(noneChar)
 }
 
 object TestBenchmark {
