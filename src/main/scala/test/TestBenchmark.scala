03197a62f38682d0f38097e0583bfc389d4cfdf5
Jason Zaugg <jzaugg@gmail.com>
2018-04-06 19:48:12 +1000

    Make the test harder for JIT/VM
    ```
    sbt:t> jmh:run -wm BULK -f1 -wi 2 -i 5
    ...
    [info] # Run complete. Total time: 00:01:44
    [info] Benchmark                   Mode  Cnt          Score         Error  Units
    [info] TestBenchmark.foldNone     thrpt    5  245724367.314 ± 3925318.428  ops/s
    [info] TestBenchmark.foldSome     thrpt    5   80545583.561 ±  352346.075  ops/s
    [info] TestBenchmark.patMatNone   thrpt    5  246221648.083 ± 1233632.523  ops/s
    [info] TestBenchmark.patMatNone2  thrpt    5  436902501.492 ± 3137665.534  ops/s
    [info] TestBenchmark.patMatSome   thrpt    5  187082256.805 ± 4103789.547  ops/s
    [info] TestBenchmark.patMatSome2  thrpt    5  301881624.485 ± 3917487.861  ops/s
    ```

--- src/main/scala/test/TestBenchmark.scala
+++ src/main/scala/test/TestBenchmark.scala
@@ -2,20 +2,37 @@ package test
 
 import org.openjdk.jmh.annotations.Benchmark
 
-import scala.io.Source
-
 class TestBenchmark {
   import TestBenchmark._
 
-  def implFold(x: Option[Char]) = x.fold(false)(_.isLower)
+  def implFold(x: Option[Char]) = {
+    x.fold(false)(Character.isLowerCase)
+    x.fold(true)(Character.isUpperCase)
+    x.fold(hashCode() > 0)(_ => true)
+    x.fold(hashCode() < 0)(_ => false)
+  }
 
-  def implPatMat(x: Option[Char]) = x match {
-    case Some(c) => c.isLower
-    case None    => false
+  def implPatMat(x: Option[Char]) = {
+    x match {
+      case Some(c) => Character.isLowerCase(c)
+      case None => false
+    }
+    x match {
+      case Some(c) => Character.isUpperCase(c)
+      case None => false
+    }
+    x match {
+      case Some(c) => true
+      case None => hashCode() > 0
+    }
+    x match {
+      case Some(c) => false
+      case None => hashCode() < 0
+    }
   }
   def implPatMat2(x: Option[Char]) = x match {
-    case None    => false
-    case Some(c) => c.isLower
+    case None => false
+    case Some(c) => Character.isLowerCase(c)
   }
 
   @Benchmark def foldSome(): Boolean = implFold(someChar)
